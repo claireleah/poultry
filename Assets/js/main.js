57188cf0ebfee4bc03fcb942b500f35a2cdad61a
claireleah <claireleah256@gmail.com>
2025-08-22 04:54:36 +0300

    more changes

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -23,3 +23,55 @@
 //         },
 //       },
 //   });
+
+ 
+      function showMessage() {
+            const email = document.getElementsByName('email').value.trim();
+                
+            
+            const contactForm = document.querySelector('.email');  
+            contactForm.addEventListener('submit',function(event){
+                event.preventDefault(); //stop the default form submission
+                const form = event.target;
+                const formData = new FormData(form); //Gathers all form data
+                const formMessageDiv = document.getElementById('formMessage');
+                formMessageDiv.textContent = 'Sending...';
+
+                fetch('send_email.php',{
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    return response.text();
+                })
+
+                .then(data => {
+                    formMessageDiv.textContent = 'Message sent successfully!';
+                    formMessageDiv.style.color ='green';
+                    form.reset();
+                    console.log(data);
+                    
+                })
+
+                .catch(error =>{
+                    formMessageDiv.textContent = 'Error sending message. Please try again.';
+                    formMessageDiv.style.color = 'red';
+                    console.error('Fetch error:',error); 
+                });
+                
+                
+
+            });
+                if (!email.includes("@") || !email.includes(".")){
+                        alert('Please enter a valid email address.');
+                        return false;
+                    }
+                    alert('Thank you for your order.')
+                    return true;
+
+                       }
+
+    
